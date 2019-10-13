21b34c61dffa03e8e625d0867dd666699d3f9272
EspinaFam <jdru0105@gmail.com>
2019-10-13 23:11:26 +0800

    Initial commit

---
+++ src/mybase/company/Main.java
@@ -0,0 +1,10 @@
+package mybase.company;
+
+public class Main {
+//0364d6fc09ec3c84409446ac47321dac2b1247c1
+//https://github.com/WannaBeOne/MyFirstGitRepo.git
+    //VCS|
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
